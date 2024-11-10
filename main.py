902004c1da4aa8266f65a1aeaec89f7b686c6ee6
myathtut-zafir <aa.htut@gmail.com>
2024-11-11 00:58:51 +0700

    feat: rag

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
 from langchain_openai import OpenAIEmbeddings,ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
 
@@ -10,3 +12,18 @@ load_dotenv()
 
 if __name__=='__main__':
     print("Read")
+    embedding=OpenAIEmbeddings()
+    llm= ChatOpenAI()
+    query="What is Pinecone in machine learning?"
+    chain=PromptTemplate.from_template(template=query)|llm
+    vectorstore=PineconeVectorStore(index_name=os.environ['INDEX_NAME'],embedding=embedding)
+    retrieval_qa_chat_prompt=hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs=create_stuff_documents_chain(llm,retrieval_qa_chat_prompt)
+    retrieval_chain=create_retrieval_chain(retriever=vectorstore.as_retriever(),combine_docs_chain=combine_docs)
+    
+    result=retrieval_chain.invoke(input={"input":query})
+    print(result)
+    
+    
+    
+    
